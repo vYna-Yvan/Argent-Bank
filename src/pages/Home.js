a5cee03c128f1ff8d3cc1c8b1ba67eb0ffe42daf
vYna-Yvan <89458360+vYna-Yvan@users.noreply.github.com>
2024-03-12 00:25:07 +0100

    supp useless import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "../style/Home.css";
 //import Banktree from "../assets/img/bank-tree.jpeg";
-import ChatIcon from "../assets/icon/icon-chat.png";
-import MoneyIcon from "../assets/icon/icon-money.png";
-import SecurityIcon from "../assets/icon/icon-security.png";
-import Item from "../components/item/item";
 import Banner from "../components/banner/banner";
 import ItemList from "../components/item/item";
 
